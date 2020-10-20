a1d01dc36e7f9f4b480b6a304e1659447496aec4
m10119002 <firman.10119002@mahasiswa.unikom.ac.id>
2020-10-21 01:52:13 +0700

    21 Rabu 2020

---
+++ src/m10119002/pbo/latihan13/Relational.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m10119002.pbo.latihan13;
+
+/**
+ *
+ * @author
+ * NAMA     : Firman Sahita
+ * KELAS    : IF-1
+ * NIM      : 10119002
+ * Deskripsi Program : Program ini berisi program Relational
+ * class
+ * 
+ */
+public class Relational {
+    
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+}
